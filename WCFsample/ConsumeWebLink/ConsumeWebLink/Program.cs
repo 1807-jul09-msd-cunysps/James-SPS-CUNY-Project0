4991b71415e9d07dbe00647472dd6b63b5bec539
James <James@LAPTOP-5E68TL9R>
2018-07-17 15:36:08 -0400

    consumed an external calculator web service

---
+++ WCFsample/ConsumeWebLink/ConsumeWebLink/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsumeWebLink.ServiceReference1;
+
+namespace ConsumeWebLink
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's use a calculator from the internet!");
+            Console.WriteLine("W");
+
+            
+            CalculatorSoapClient client = new CalculatorSoapClient();
+            Console.WriteLine(client.Add(21, 22));
+            Console.Read();
+        }
+    }
+}
