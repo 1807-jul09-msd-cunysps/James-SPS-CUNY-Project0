b76849f68b623ff59a2499121ffa7f215314b8af
James <James@LAPTOP-5E68TL9R>
2018-07-30 12:23:13 -0400

    1st commit for docing challenge, alerts the elements

---
+++ AJAXcodingChallenge_day16/AJAXcodingChallenge/Pages/JavaScript.js
@@ -0,0 +1,14 @@
+﻿var requestURL = 'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json';
+var request = new XMLHttpRequest(); 
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+request.onload = function () {
+    var superHeroes = request.response;
+    for (var x in superHeroes) {
+        alert(x+": "+superHeroes[x]);
+    }
+//    document.createElement(h1, superHeroes['squadName']);
+    populateHeader(superHeroes);
+    showHeroes(superHeroes);
+}
